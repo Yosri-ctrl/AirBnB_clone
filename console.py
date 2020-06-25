e964a7c374e9aac387e2b4705c2f18a8f4dd55bc
FouedDadi <foued.dadii@gmail.com>
2020-06-25 18:42:53 +0100

    console

--- console.py
+++ console.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
-import itertools
-from os import sys
+import cmd
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb)"
+    def do_quit(self, arg):
+        return True
 
-def user_input():
-    for arg in itertools.count():
-        try:
-            yield arg, input('[%s]: ' % arg)
-        except KeyboardInterrupt:
-            pass
-        except EOFError:
-            break
-
-def main():
-    for arg, input in user_input():
-        pass
+    def do_EOF(self, arg):
+        return True
 
 if __name__ == '__main__':
-    main()
+    HBNBCommand().cmdloop()
