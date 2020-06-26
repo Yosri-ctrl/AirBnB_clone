fa79b86ef494840e797c8428687740a53eb6d409
Yosri-ctrl <yosribouabid@gmail.com>
2020-06-26 18:46:06 +0100

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -14,15 +14,38 @@ class BaseModel():
     updated_at: datetime - assign with the current datetime when an instance is created
     and it will be updated every time you change your object
     """
-    id = str(uuid.uuid4())
-    created_at = str(datetime.now())
-    updated_at = str(datetime.now())
+    
+    f = "%Y-%m-%dT%H:%M:%S.%f"
+    def __init__(self, *args, **kwargs):
+        """
+        """
+        if kwargs is not None:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            for i, j in kwargs.items():
+                if (i == "created_at" or i == "updated_at"):
+                    j = datetime.strptime(j, self.f)
+                    setattr(self, i, j)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
 
     def __str__(self):
-        return "{} ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        """
+        """
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        BaseModel.updated_at = str(datetime.now())
+        """
+        """
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        return {"__class__": type(self).__name__, "id": self.id, "created_at": self.created_at, "updated_at": self.updated_at, self.__dict__}
+        """
+        """
+        self.created_at = datetime.now().isoformat("T")
+        self.updated_at = datetime.now().isoformat("T")
+        dict = self.__dict__
+        dict.update({'__class__': type(self).__name__})
+        return dict
