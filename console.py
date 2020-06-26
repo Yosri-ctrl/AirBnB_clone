d57133ae89c57db169a5709ebb95462aea350ed7
FouedDadi <foued.dadii@gmail.com>
2020-06-26 20:14:21 +0100

    console update

--- console.py
+++ console.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 import cmd
 from os import sys
+from models.base_model import BaseModel
+
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
+    dict = {'BaseModel': BaseModel}
     def do_quit(self, arg):
         """
     press quit to exit the program
@@ -19,6 +22,18 @@ class HBNBCommand(cmd.Cmd):
     execute nothing if a line is empty
         """
         pass
+    def do_create(self, arg):
+        """
+    create a new instance and print the id
+        """
+        if len(arg) is 0:
+            print("** class name missing **")
+        elif arg not in self.dict.keys():
+            print("** class doesn't exist **")
+        else:
+            created = self.dict[arg]()
+            created.save()
+            print(created.id)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
