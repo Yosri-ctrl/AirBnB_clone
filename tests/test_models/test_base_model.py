9a561ee73eb789a4cec7543f979c9772c922a142
Yosri-ctrl <yosribouabid@gmail.com>
2020-07-01 15:11:19 +0100

    test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,7 +7,6 @@ import uuid
 import datetime
 from models import storage
 import unittest
-from models.user import User
 
 
 class TestBaseModel(unittest.TestCase):
@@ -96,47 +95,5 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(test.number, 99)
         self.assertEqual(test_dict, f)
 
-        def test_user(self):
-            """
-        test user
-        """
-        user = User()
-        self.assertIsInstance(user, User)
-
-    def test_first_name(self):
-        """
-        first name
-        """
-        user = User()
-        user.first_name = "hello"
-        self.assertIsInstance(user.first_name, str)
-        self.assertEqual(user.first_name, "hello")
-
-    def test_last_name(self):
-        """
-        last name
-        """
-        user = User()
-        user.last_name = "world"
-        self.assertIsInstance(user.last_name, str)
-        self.assertEqual(user.last_name, "world")
-
-    def test_email(self):
-        """
-        test email
-        """
-        user = User()
-        user.email = "airbnb@helloworld.com"
-        self.assertIsInstance(user.email, str)
-        self.assertEqual(user.email, "airbnb@helloworld.com")
-
-    def test_root(self):
-        """
-        test user
-        """
-        user = User()
-        user.password = "root"
-        self.assertIsInstance(user.password, str)
-        self.assertEqual(user.password, "root")
 if __name__ == '__main__':
     unittest.main()
