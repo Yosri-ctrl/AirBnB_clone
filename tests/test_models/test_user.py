5e80d6a133afa28819079ede1022554c71cda3b9
Yosri-ctrl <yosribouabid@gmail.com>
2020-07-01 15:04:16 +0100

    test

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -5,9 +5,6 @@ Test For User
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
-import uuid
-import datetime
-from models import storage
 import unittest
 
 
@@ -16,32 +13,47 @@ class TestUser(unittest.TestCase):
     test the user class
     """
     def test_user(self):
+        """
+        test user
+        """
         user = User()
         self.assertIsInstance(user, User)
-    """
+    
     def test_first_name(self):
+        """
+        first name
+        """
         user = User()
         user.first_name = "hello"
         self.assertIsInstance(user.first_name, str)
         self.assertEqual(user.first_name, "hello")
 
     def test_last_name(self):
+        """
+        last name
+        """
         user = User()
         user.last_name = "world"
         self.assertIsInstance(user.last_name, str)
         self.assertEqual(user.last_name, "world")
 
     def test_email(self):
+        """
+        test email
+        """
         user = User()
         user.email = "airbnb@helloworld.com"
         self.assertIsInstance(user.email, str)
         self.assertEqual(user.email, "airbnb@helloworld.com")
 
     def test_root(self):
+        """
+        test user
+        """
         user = User()
         user.password = "root"
         self.assertIsInstance(user.password, str)
         self.assertEqual(user.password, "root")
-    """
+
 if __name__ == '__main__':
     unittest.main()
