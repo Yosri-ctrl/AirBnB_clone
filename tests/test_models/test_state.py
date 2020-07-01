e63699f5ae7d98501a6ecf6fc67d1c91dfa1cb79
Yosri-ctrl <yosribouabid@gmail.com>
2020-07-01 15:19:35 +0100

    test

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -11,9 +11,12 @@ class TestStateMethod(unittest.TestCase):
     test state class
     """
     def test_state(self):
+        """
+        test state
+        """
         state = State()
         self.assertIsInstance(state, State)
-
+        self.assertIsInstance(state.name, str)
 
 if __name__ == '__main__':
     unittest.main()
